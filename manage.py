45082a2192a9bb2cd534bb15a7ff8e8aafbf40bd
mk1123 <khattar.manan@gmail.com>
2019-05-26 06:12:02 -0700

    should fix some stuff

--- manage.py
+++ manage.py
@@ -1,20 +1,18 @@
 from flask_script import Manager
-from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
 
 from app import app
 
 manager = Manager(app)
-
+account_sid = 'AC92cbcdb460fcb1606c06fcf92e013436'
+auth_token = '509f7fea616ef52459984f9bd6271bcc'
+client = Client(account_sid, auth_token)
 
 @manager.command
 def update():
-    resp = MessagingResponse()
-
-    # Add a message
-    resp.message(
-        "Hey man, how's your day been?")
-
-    return str(resp)
+    message = client.messages.create(body="Hey man. How's your day going?",
+                                     from_='+14152377478',
+                                     to='+19256678140')
 
 
 if __name__ == "__main__":
