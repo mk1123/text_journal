8a38580265fd81383daeb032d42aa7fdd351d0bd
mk1123 <khattar.manan@gmail.com>
2019-05-31 19:48:26 -0700

    wtf

--- app.py
+++ app.py
@@ -51,6 +51,8 @@ def login():
         sys.stdout.flush()
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            print("in here")
+            sys.stdout.flush()
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
