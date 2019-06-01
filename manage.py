e921431c6be52d560f1f939a6aedbfafe771b8be
mk1123 <khattar.manan@gmail.com>
2019-05-31 21:19:40 -0700

    new user stuff

--- manage.py
+++ manage.py
@@ -19,17 +19,14 @@ manager.add_command('db', MigrateCommand)
 account_sid = 'AC92cbcdb460fcb1606c06fcf92e013436'
 auth_token = '509f7fea616ef52459984f9bd6271bcc'
 client = Client(account_sid, auth_token)
-
-client_phones = ['+19256678140', '+19253536746', '+19255575551', '+19253519739']
-
-p=argparse.ArgumentParser()
+twilio_phone = '+14152377478'
 
 @manager.command
 def update():
     client_phones = db.session.query(User.phone_num).all()
     for client_string in client_phones:
         message = client.messages.create(body="Hey man. How's your day going?",
-                                        from_='+14152377478',
+                                        from_=twilio_phone,
                                         to=client_string)
         
 @manager.command
@@ -37,7 +34,18 @@ def user_setup(phone_num, name):
     new_user = User(phone_num, name)
     db.session.add(new_user)
     db.session.commit()
-    return db.session.query(User).all()
+    first_paragraph = "Welcome to Manan's Journal! I'm Jiminy. I'll ask you how your day's going every day\
+                                     at 9PM, and feel free to respond however you want. I'll store your response, \
+                                     so you can keep track of all your thoughts!"
+    second_paragraph = "To see all your previous entries, go to http://manansjournal.herokuapp.com."
+    third_paragraph = "You'll have to log in! Your username is " + new_user.username + ", but you'll have to \
+                                    set your password. To set your password, send me a message like this:\n \
+                                    password: <password>\n \
+                                    where <password> is your new password. I'll let you know once your password's been saved!"
+
+    client.messages.create(from_=twilio_phone, to=phone_num, body=first_paragraph)
+    client.messages.create(from_=twilio_phone, to=phone_num, body=second_paragraph)
+    client.messages.create(from_=twilio_phone, to=phone_num, body=third_paragraph)
 
 @manager.command
 def reset_properties():
