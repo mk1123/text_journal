bf5c926d8f2721284b50ef58a2e44fb0f64b23e2
mk1123 <khattar.manan@gmail.com>
2019-05-31 19:06:11 -0700

    tatti

--- app.py
+++ app.py
@@ -43,8 +43,6 @@ def is_safe_url(target):
     
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated():
-        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
