f064990f4fe08b5e367b20f0ff0b360c18ddd2bd
mk1123 <khattar.manan@gmail.com>
2019-08-09 19:02:14 -0700

    security improvements

--- manage.py
+++ manage.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+from models import User, Entry
 from flask_script import Manager
 from twilio.rest import Client
 from flask_migrate import Migrate, MigrateCommand
@@ -9,54 +11,60 @@ from app import app, db
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-from models import User, Entry
-from datetime import datetime, timedelta
-
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+account_sid = os.environ['account_sid']
+auth_token = os.environ['auth_token']
 manager.add_command('db', MigrateCommand)
-
-account_sid = 'AC92cbcdb460fcb1606c06fcf92e013436'
-auth_token = '509f7fea616ef52459984f9bd6271bcc'
 client = Client(account_sid, auth_token)
 twilio_phone = '+14152377478'
-list_of_messages = ['Hey! How are you doing today?', 'Greetings! Do you have a moment to talk about your day?', 'Hi! Tell me a pertinent fact about your day!']
+list_of_messages = ['Hey! How are you doing today?',
+                    'Greetings! Do you have a moment to talk about your day?', 'Hi! Tell me a pertinent fact about your day!']
+can
 
 
 @manager.command
 def update2():
-    active_users = db.session.query(User).filter(User.is_deleted == False).all()
+    active_users = db.session.query(User).filter(
+        User.is_deleted == False).all()
     print(active_users)
     for user in active_users:
         client_string = user.phone_num
         # print(client_string)
         # print(user.name)
-        user_exists = db.engine.execute("select date from entries where name='" + user.name + "'order by date desc").first()
+        user_exists = db.engine.execute(
+            "select date from entries where name='" + user.name + "'order by date desc").first()
         if user_exists:
             latest_entry = user_exists.date
             # print(latest_entry)
             if datetime.now() - latest_entry > timedelta(days=3):
                 message = client.messages.create(body="Hey, haven't seen you in a while! Feel free to just hit me up any time you want to say something noteworthy or just pertinent. Future you won't regret it!",
-                                            from_=twilio_phone,
-                                            to=client_string)
-                message = client.messages.create(body="Of course, if you don't want to hear from me anymore, just type 'unsubscribe' and I'll deactivate your account.",from_=twilio_phone,to=client_string)
+                                                 from_=twilio_phone,
+                                                 to=client_string)
+                message = client.messages.create(
+                    body="Of course, if you don't want to hear from me anymore, just type 'unsubscribe' and I'll deactivate your account.", from_=twilio_phone, to=client_string)
             else:
-                message = client.messages.create(body=list_of_messages[randint(0, 2)],from_=twilio_phone,to=client_string)
+                message = client.messages.create(
+                    body=list_of_messages[randint(0, 2)], from_=twilio_phone, to=client_string)
         else:
-            message = client.messages.create(body=list_of_messages[randint(0, 2)],from_=twilio_phone,to=client_string)
-        
+            message = client.messages.create(
+                body=list_of_messages[randint(0, 2)], from_=twilio_phone, to=client_string)
+
+
 @manager.command
 def update():
-    active_users = db.session.query(User).filter(User.is_deleted == False).all()
+    active_users = db.session.query(User).filter(
+        User.is_deleted == False).all()
     # print(active_users)
     for user in active_users:
         client_string = user.phone_num
         # print(client_string)
         # print(user.name)
-        message = client.messages.create(body=list_of_messages[randint(0, 2)],from_=twilio_phone,to=client_string)
-            
-        
+        message = client.messages.create(
+            body=list_of_messages[randint(0, 2)], from_=twilio_phone, to=client_string)
+
+
 @manager.command
 def user_setup(phone_num, name):
     new_user = User(phone_num, name)
@@ -72,11 +80,17 @@ def user_setup(phone_num, name):
     fifth_paragraph = "where <password> is your new password. Your password is SHA256 encrypted, so the only \
                                     person that can see it is you."
 
-    client.messages.create(from_=twilio_phone, to=phone_num, body=first_paragraph)
-    client.messages.create(from_=twilio_phone, to=phone_num, body=second_paragraph)
-    client.messages.create(from_=twilio_phone, to=phone_num, body=third_paragraph)
-    client.messages.create(from_=twilio_phone, to=phone_num, body=fourth_paragraph)
-    client.messages.create(from_=twilio_phone, to=phone_num, body=fifth_paragraph)
+    client.messages.create(
+        from_=twilio_phone, to=phone_num, body=first_paragraph)
+    client.messages.create(
+        from_=twilio_phone, to=phone_num, body=second_paragraph)
+    client.messages.create(
+        from_=twilio_phone, to=phone_num, body=third_paragraph)
+    client.messages.create(
+        from_=twilio_phone, to=phone_num, body=fourth_paragraph)
+    client.messages.create(
+        from_=twilio_phone, to=phone_num, body=fifth_paragraph)
+
 
 @manager.command
 def reset_properties():
